99ebe711e90d4af58b6e7429ae5df7cc6fdfe266
davon92 <davon92@gmail.com>
2025-04-03 19:22:27 -0400

    Updated Assets

---
+++ Source/MP_ARENA/Private/PowerUpBase.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PowerUpBase.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+APowerUpBase::APowerUpBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	RootComponent = Collision;
+	Collision->SetCollisionProfileName("OverlapAllDynamic");
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &APowerUpBase::OnOverlapBegin);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	
+	
+}
+
+// Called when the game starts or when spawned
+void APowerUpBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APowerUpBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APowerUpBase::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,bool bFromSweep, const FHitResult& SweepResult)
+{
+	// Basic interaction — override in BP or subclasses
+	UE_LOG(LogTemp, Warning, TEXT("PowerUp picked up by: %s"), *OtherActor->GetName());
+	Destroy();
+}
+
---
+++ Source/MP_ARENA/Public/PowerUpBase.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PowerUpBase.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class MP_ARENA_API APowerUpBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APowerUpBase();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USphereComponent* Collision;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+								UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+								bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
